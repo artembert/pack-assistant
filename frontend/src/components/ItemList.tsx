d661544b0241224ae97623362028a6e011836c3d
Artem Beresnev <artembert@gmail.com>
2025-06-02 20:32:08 +0300

    Preserve items order

--- frontend/src/components/ItemList.tsx
+++ frontend/src/components/ItemList.tsx
@@ -8,11 +8,13 @@ export interface ItemListProps {
 }
 
 export function ItemList({ items, onCheck }: ItemListProps) {
+  const sortedItems = [...items].sort((a, b) => (a.name > b.name ? 1 : -1))
   return (
     <List sx={{ p: 0 }} dense>
-      {items.map((item) => (
-        <ItemRow key={item.id} item={item} onCheck={onCheck} />
-      ))}
+      {sortedItems?.length > 0 &&
+        sortedItems.map((item) => (
+          <ItemRow key={item.id} item={item} onCheck={onCheck} />
+        ))}
     </List>
   )
 }
