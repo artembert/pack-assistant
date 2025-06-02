02f4130d0954a0946c32fa5625b4903c3615c5a4
Artem Beresnev <artembert@gmail.com>
2025-06-02 18:03:08 +0300

    Reformat generated code

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -3,14 +3,14 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 const config: CodegenConfig = {
   schema: '../backend/src/main/resources/schema/schema.graphqls',
   documents: ['src/**/*.tsx', 'src/**/*.ts'],
+  hooks: {
+    afterAllFileWrite: ['prettier --write', 'eslint --fix']
+  },
   generates: {
     './src/__generated__/': {
       preset: 'client',
       presetConfig: {
         gqlTagName: 'gql'
-      },
-      hooks: {
-        afterAllFileWrite: ['eslint --fix']
       }
     },
     './src/__generated__/types.ts': {
